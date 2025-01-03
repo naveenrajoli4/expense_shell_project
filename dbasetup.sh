f391cb15c772e284c84d8c672b4357cfc3f9d79b
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-03 21:39:12 +0530

    oih

---
+++ dbasetup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+mkdir -p "/var/log/expense_log"
+
+LOGS_FOLDER="/var/log/expense_log"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%d-%m-%Y-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE() {
+    if [ $1 -ne 0]
+    then
+        echo -e "$2 .....FAILURE"
+        exit 1
+    else
+        echo -e "$2-------SUCCESS"
+    fi
+}
+
+echo "script started at: $TIMESTAMP"
+
+$USERID 
+VALIDATE $? "rootuser"
+
+

04022f2cfcd6f54bc1ebc7eef9febfee2b7e4af1
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-03 19:06:53 +0530

    commiting changes

---
+++ rnk02.sh
@@ -0,0 +1,39 @@
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2..  $R failure $N "
+        exit 1
+    else
+        echo -e "$2... $G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R You should a root user to run this script $N "
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    dnf install mysql-server -y
+    VALIDATE $? "installing-mysql"
+else
+    echo -e "$Y MYSQL is already installed $N"
+fi
+
+systemctl enable mysqld
+VALIDATE $? "enable-mysqld-service"
+
+
+systemctl start mysqld
+VALIDATE $? "start-mysqld-service"
---
+++ rnk03.sh
@@ -0,0 +1,46 @@
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/scriptlogs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%d-%m-%Y-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2..  $R failure $N "
+        exit 1
+    else
+        echo -e "$2... $G SUCCESS $N"
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R You should a root user to run this script $N "
+    exit 1
+fi
+
+dnf list installed mysql &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then 
+    dnf install mysql-server -y &>>$LOG_FILE_NAME
+    VALIDATE $? "installing-mysql"
+else
+    echo -e "$Y MYSQL is already installed $N"
+fi
+
+systemctl enable mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "enable-mysqld-service"
+
+
+systemctl start mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "start-mysqld-service"
